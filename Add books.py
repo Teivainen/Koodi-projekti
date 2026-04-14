90463d7c9d69fff7ef6181b81dc85a271753d897
Emma Teivainen <emma.teivainen@edu.turkuamk.fi>
2026-04-14 19:24:51 +0300

    testasin että toimii

--- Add books.py
+++ Add books.py
@@ -2,10 +2,13 @@ libary = []
 
 def add_books(isbn, title, author, genre, copies):
     book = {
-        "isbn": isbn,
-        "title": title,
-        "author": author,
-        "genre": genre,
-        "copies": copies
+        "ISBN": isbn,
+        "Title": title,
+        "Author": author,
+        "Genre": genre,
+        "Copies": copies
     }
     libary.append(book)
+
+add_books("978-951-1", "Kotiapulainen valvoo", "Freida McFadden", "Psychological thriller", 12)
+print(libary)
