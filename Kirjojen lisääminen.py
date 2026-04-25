7a8a2a7d00e22e8454f65ae55eb05582dd9aa2be
Emma Teivainen <emma.teivainen@edu.turkuamk.fi>
2026-04-25 16:30:48 +0300

    Nyt olisi parempi versio

--- Kirjojen lisääminen.py
+++ Kirjojen lisääminen.py
@@ -1,1 +1,39 @@
-libary = []
+libary = [
+    {
+        "isbn": "978-951-1-49901-5",
+        "title": "Opettaja",
+        "author": "Freida McFadden",
+        "genre": "Psychological thriller",
+        "copies": "3"
+    },
+    {
+        "isbn": "978-951-1-48900-9",
+        "title": "Työkaveri",
+        "author": "Freida McFadden",
+        "genre": "Psychological thriller",
+        "copies": "1"
+    }
+]
+
+isbn = input("Please enter ISBN: ")
+title = input("Please enter title: ")
+author = input("Please enter author: ")
+genre = input("Please enter genre: ")
+copies = input("Please enter the number of available copies: ")
+
+libary.append({
+    "isbn": isbn,
+    "title": title,
+    "author": author,
+    "genre": genre,
+    "copies": copies
+
+})
+
+print("Book added successfully!")
+
+for book in libary:
+    print(
+        f"{book["title"]} by {book["author"]}, {book["isbn"]}" 
+        f"{book["genre"]}, available copies: {book["copies"]}"
+    )
