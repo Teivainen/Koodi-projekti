d888c9f1da9d99454d2309c48b97f1b6be50ab2d
Viivi Varis <viivi.varis@edu.turkuamk.fi>
2026-04-28 15:15:48 +0300

    korjaus

--- search_books.py
+++ search_books.py
@@ -2,13 +2,16 @@
 library = []
 
 #hakukenttä
-search = input("Search a book by its name or author: ").lower()
+def search_books(library):
+    search = input("Search a book by its name or author: ").lower()
 found = False
 
 for title, author in library: #hakee joko kirjan tai kirjailijan nimellä
-    if search in title.lower() or search in author.lower():
+    if search_books in title.lower() or search_books in author.lower():
         print("Found:", title, "-", author)
         found = True
 
 if not found:
     print("No results.")
+
+search_books(library)
