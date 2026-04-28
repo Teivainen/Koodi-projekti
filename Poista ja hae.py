b428e755bd19813218473452839bd83a7e40e22e
Huda <sunemail@example.com>
2026-04-28 15:19:29 +0300

    Added remove and show books

--- Poista ja hae.py
+++ Poista ja hae.py
@@ -1,22 +1,4 @@
 
-library = [
-    {
-        "isbn": "978-951-1-49901-5",
-        "title": "Opettaja",
-        "author": "Freida McFadden",
-        "genre": "Psychological thriller",
-        "copies": "3"
-    },
-    {
-        "isbn": "978-951-1-48900-9",
-        "title": "Työkaveri",
-        "author": "Freida McFadden",
-        "genre": "Psychological thriller",
-        "copies": "1"
-    }
-]
-
-
 def show_all_books():
     if not library:
         print("No books in the library.")
@@ -48,7 +30,10 @@ while True:
     print("\nLibrary system")
     print("1. Show all books")
     print("2. Remove a book")
-    print("3. Exit")
+    print("3. Add a books")
+    print("4. Borrow book")
+    print("5. Return book")
+    print("6. Exit")
 
     choice = input("Choose an option: ")
 
@@ -57,6 +42,12 @@ while True:
     elif choice == "2":
         remove_book()
     elif choice == "3":
+        add_books()
+    elif choice == "4":
+        borrow_book()
+    elif choice == "5":
+        return_book()
+    elif choice == "4":
         print("Goodbye!")
         break
     else:
